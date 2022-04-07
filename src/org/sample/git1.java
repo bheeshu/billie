9ec61bb294336b02fc3cf170209464357b143e53
bheeshu <kishorkd1612@gmail.com>
2022-04-07 17:08:32 +0530

    JD done his coding

--- src/org/sample/git1.java
+++ src/org/sample/git1.java
@@ -1,8 +1,13 @@
 package org.sample;
 
 public class git1 {
+
+	private void add() {
+		System.out.println("add1");
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("done");
 
 	}
